0355f574152ec1b0eff8e2ae3b91212a684707ca
mehdi bouziane <b.bouziane92@gmail.com>
2023-04-03 15:49:34 +0200

    setup home page

---
+++ src/Controller/UrlsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UrlsController extends AbstractController
+{
+    #[Route('/', name: 'app_url_create')]
+    public function create(): Response
+    {
+        return $this->render('urls/create.html.twig', []);
+    }
+}
