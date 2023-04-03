c287996c12286a0e8a0344c337b8b7d309fa98ae
mehdi bouziane <b.bouziane92@gmail.com>
2023-04-03 16:41:48 +0200

    Add validation form

--- src/Controller/UrlsController.php
+++ src/Controller/UrlsController.php
@@ -2,23 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UrlsController extends AbstractController
 {
     #[Route('/', name: 'app_url_create')]
-    public function create(): Response
+    public function create(Request $request): Response
     {
+
         $form = $this->createFormBuilder()
             ->add('original', null, [
                 'label' => false,
                 'attr' => [
-                    'placeholder' => 'Enter your Url !'
+                    'placeholder' => 'Enter the URL to shorten here !'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'You need to enter an URL']),
+                    new Url(['message' => 'The URL entered is invalid'])
                 ]
             ])
             ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd('Traitement..');
+        }
 
 
 
