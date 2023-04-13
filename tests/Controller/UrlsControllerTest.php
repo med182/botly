e88c6d9ae28d75a46614c3ce52e831976aa5c9e2
mehdi bouziane <b.bouziane92@gmail.com>
2023-04-13 17:28:00 +0200

    Add test for form logic

--- tests/Controller/UrlsControllerTest.php
+++ tests/Controller/UrlsControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UrlsControllerTest extends WebTestCase
 {
-    public function testCreate(): void
+    public function testHomepage_should_display_url_shortener_form(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
@@ -17,5 +17,17 @@ class UrlsControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'The best Url shortener out there!');
         $this->assertSelectorExists('input[id="form_original"]');
         $this->assertSelectorExists('input[placeholder="Enter the URL to shorten here !"]');
+    }
+
+    public function testForm_should_work_with_valid_data()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $form = $crawler->filter('form')->form();
+        $client->submit($form, ['form[original]' => 'https://python.org']);
+
+        $this->assertResponseRedirects();
     }
 }
